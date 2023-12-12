95cc94028b1032f718b4facfd159414de8fce235
danielvazquezguerra <daniel.vazquezquerra@gmail.com>
2023-12-12 22:13:06 +0100

    iniciando estilos

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native';
 
 export default function App() {
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <TouchableWithoutFeedback onPress={()=> Alert.alert('Hemos tocado el texto')}>
+        <Text>Hola Daniel!</Text> 
+      </TouchableWithoutFeedback>
     </View>
+
   );
 }
 
